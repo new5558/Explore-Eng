ec3ca0cd6571c15248b3e1659d18f398fc1ce24b
new5558 <new17353@gmail.com>
2019-04-03 23:41:07 +0700

    fixed?

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,11 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/static/icons/site.webmanifest" />
+          <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="apple-mobile-web-app-title" content="Test" />
           <meta name="application-name" content="Test" />
           <meta name="msapplication-TileColor" content="#da532c" />
