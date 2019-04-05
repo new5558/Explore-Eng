e6d06d85035fea283be99a14bc6ae7036348b5d0
new5558 <new17353@gmail.com>
2019-04-05 11:51:49 +0700

    fix position of close search btn

--- components/home/SearchBar.jsx
+++ components/home/SearchBar.jsx
@@ -9,10 +9,7 @@ export default class extends Component {
             <div className="mx-3 px-3 h-12 w-full bg-white shadow-md items-center rounded-lg flex">
                 <SearchIcon />
                 <input placeholder="Search ECO ATM Location" value={value} onChange={onChange} className="h-12 py-2 w-full mx-2" />
-                {/* <div >clear</div> */}
-                <div onClick={closeSearch}>
-                    <CloseIcon isSearching={isSearching} />
-                </div>
+                <CloseIcon onClick={closeSearch} isSearching={isSearching} />
             </div>
         );
     }
