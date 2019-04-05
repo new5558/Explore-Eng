162a65930b6466fc828ffcb349fb5c72d21f99fa
new5558 <new17353@gmail.com>
2019-04-05 23:56:04 +0700

    try service worker

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withCSS = require('@zeit/next-css')
 const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack')
+const withOffline = require('next-offline')
 
-module.exports = withCSS({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-    return config
-  }
-})
+module.exports = withOffline(
+  withCSS({
+    webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+      return config
+    }
+  }))
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
-const express = require('express')
+// const express = require('express')
+// const next = require('next')
+// require('dotenv').config();
+
+// const app = next({ dev: process.env.NODE_ENV !== 'production' })
+// const handle = app.getRequestHandler();
+// (async () => {
+//   await app.prepare()
+//   const server = express()
+//   server.get('*', (req, res) => handle(req, res))
+
+//   await server.listen(process.env.PORT || 3000)
+//   console.log('> Ready on http://localhost:3000')
+// })()
+
+const { createServer } = require('http')
+const { join } = require('path')
+const { parse } = require('url')
 const next = require('next')
 require('dotenv').config();
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
-const handle = app.getRequestHandler();
-(async () => {
-  await app.prepare()
-  const server = express()
-  server.get('*', (req, res) => handle(req, res))
+const handle = app.getRequestHandler()
 
-  await server.listen(process.env.PORT || 3000)
-  console.log('> Ready on http://localhost:3000')
-})()
+app.prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true)
+      const { pathname } = parsedUrl
+
+      // handle GET request to /service-worker.js
+      if (pathname === '/service-worker.js') {
+        const filePath = join(__dirname, '.next', pathname)
+
+        app.serveStatic(req, res, filePath)
+      } else {
+        handle(req, res, parsedUrl)
+      }
+    })
+      .listen(3000, () => {
+        console.log(`> Ready on http://localhost:${3000}`)
+      })
+  })
