cd411ec9e677d8273da1d62c140fcd1e01d2b4bf
new5558 <new17353@gmail.com>
2019-04-07 03:12:02 +0700

    print debug message from camera

--- pages/account.js
+++ pages/account.js
@@ -4,14 +4,25 @@ import 'react-html5-camera-photo/build/css/index.css';
 
 class Account extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state ={
+            debug: null,
+        }
+    }
+
     onTakePhoto = (dataUri) => {
         // Do stuff with the dataUri photo...
-        console.log('takePhoto', dataUri);
+        // console.log('takePhoto', dataUri);
+        this.setState({
+            debug: dataUri
+        })
     }
 
     render() {
         return (
             <div>
+                {this.state.debug}
                 <Camera
                     onTakePhoto={(dataUri) => this.onTakePhoto(dataUri)}
                     idealFacingMode={FACING_MODES.ENVIRONMENT}
