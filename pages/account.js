0816129af5e41b08794a444c2db12896dd1d30eb
new5558 <new17353@gmail.com>
2019-04-07 02:48:32 +0700

    added camera

--- pages/account.js
+++ pages/account.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
 class Account extends Component {
-    render() {
-        return(
-            <div>
 
+    onTakePhoto = (dataUri) => {
+        // Do stuff with the dataUri photo...
+        console.log('takePhoto', dataUri);
+    }
+
+    render() {
+        return (
+            <div>
+                <Camera
+                    onTakePhoto={(dataUri) => { this.onTakePhoto(dataUri); }}
+                />
             </div>
         )
     }
