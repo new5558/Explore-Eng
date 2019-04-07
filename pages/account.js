8f962db749dd761338e574eeece5172456b71a25
new5558 <new17353@gmail.com>
2019-04-08 03:41:04 +0700

    fix login error

--- pages/account.js
+++ pages/account.js
@@ -14,6 +14,7 @@ class Account extends Component {
     facebookLogin = () => {
         var provider = new firebase.auth.FacebookAuthProvider();
         firebase.auth().signInWithRedirect(provider);
+        console.log('firenase')
         firebase.auth().getRedirectResult().then((result) => {
             if (result.credential) {
                 // This gives you a Facebook Access Token. You can use it to access the Facebook API.
@@ -22,7 +23,6 @@ class Account extends Component {
             }
             // The signed-in user info.
             var user = result.user;
-            this.setUserInfo(user, true);
         }).catch((error) => {
             // Handle Errors here.
             var errorCode = error.code;
@@ -31,9 +31,10 @@ class Account extends Component {
             var email = error.email;
             // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-            this.setState({
-                loginErrorMessage: errorMessage,
-            })
+            // console.log('error', errorMessage)
+            // this.setState({
+            //     loginErrorMessage: errorMessage,
+            // })
             // ...
         });
     }
@@ -48,59 +49,65 @@ class Account extends Component {
         })
     }
 
+    componentDidUpdate() {
+        console.log(this.state)
+    }
+
     render() {
         // console.log(this.props.userInfo.displayName, this.props)
         return (
             <div className="flex items-center flex-col justify-start pt-2 h-screen">
-                {
-                    this.props.isLogin === null
-                        ?
-                        (
-                            <div className="lds-facebook relative">
-                                <div style={{ background: "grey" }}></div>
-                                <div style={{ background: "grey" }}></div>
-                                <div style={{ background: "grey" }}></div>
-                            </div>
-                        )
-                        :
-                        (
-                            this.props.isLogin
-                                ?
-                                (
-                                    <React.Fragment>
-                                        <div className="flex items-center">
-                                            Welcome:&nbsp;
+                <div className="flex flex-col h-full items-center justify-center pb-32">
+                    {
+                        this.props.isLogin === null
+                            ?
+                            (
+                                <div className="lds-facebook relative">
+                                    <div style={{ background: "grey" }}></div>
+                                    <div style={{ background: "grey" }}></div>
+                                    <div style={{ background: "grey" }}></div>
+                                </div>
+                            )
+                            :
+                            (
+                                this.props.isLogin
+                                    ?
+                                    (
+                                        <React.Fragment>
+                                            <div className="flex items-center">
+                                                Welcome:&nbsp;
                                             <span className="text-blue">
-                                                {this.props.userInfo.displayName}
-                                            </span>
-                                        </div>
-                                        <div className="m-2 px-8 py-2 flex items-end shadow text-white" style={{ borderRadius: "1rem", background: "linear-gradient(to right, #ff9933 0%, #ff0000 100%)" }} onClick={this.logout}>
-                                            <span>
+                                                    {this.props.userInfo.displayName}
+                                                </span>
+                                            </div>
+                                            <div className="m-2 px-8 py-2 flex items-end shadow text-white" style={{ borderRadius: "1rem", background: "linear-gradient(to right, #ff9933 0%, #ff0000 100%)" }} onClick={this.logout}>
+                                                <span>
                                                     LOGOUT
                                             </span>
-                                        </div>
-                                    </React.Fragment>
-                                )
-                                :
-                                (
-                                        <div className="flex flex-col h-full items-center justify-center pb-32">
-                                    <img src="../static/image/iphonexspacegrey_portrait.jpg" style={{maxHeight: "60vh", height: "100%", maxWidth: "auto"}}/>
-                                        <span className="mt-2">
-                                            To Earn Points
+                                            </div>
+                                        </React.Fragment>
+                                    )
+                                    :
+                                    (
+                                        <React.Fragment>
+                                            <img src="../static/image/iphonexspacegrey_portrait.jpg" style={{ maxHeight: "60vh", height: "100%", maxWidth: "auto" }} />
+                                            <span className="mt-2">
+                                                To Earn Points
                                         </span>
-                                    <div className="m-2 px-2 mt-2 py-2 flex items-end shadow text-white" style={{ borderRadius: "1rem", background: "linear-gradient(to right, #000099 0%, #0099cc 100%)" }} onClick={this.facebookLogin}>
-                                        <div className="w-8 h-8 mr-6">
-                                            <FacebookIcon fill="#FFFFFF" />
-                                        </div>
-                                        <span className="mr-4">
-                                            LOGIN WITH FACEBOOK
+                                            <div className="m-2 px-2 mt-2 py-2 flex items-end shadow text-white" style={{ borderRadius: "1rem", background: "linear-gradient(to right, #000099 0%, #0099cc 100%)" }} onClick={this.facebookLogin}>
+                                                <div className="w-8 h-8 mr-6">
+                                                    <FacebookIcon fill="#FFFFFF" />
+                                                </div>
+                                                <span className="mr-4">
+                                                    LOGIN WITH FACEBOOK
                                         </span>
-                                    </div>
-                                        </div>
-                                )
-                        )
-                }
-            </div>
+                                            </div>
+                                        </React.Fragment>
+                                    )
+                            )
+                    }
+                </div>
+            </div >
         )
     }
 }
