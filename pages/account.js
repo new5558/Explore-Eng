7695fb12f424fb5187ec0029a75c9a430b09d043
new5558 <new17353@gmail.com>
2019-04-07 19:08:02 +0700

    remove react webcam

--- pages/account.js
+++ pages/account.js
@@ -1,39 +1,14 @@
 import React, { Component } from 'react';
-import Camera, { FACING_MODES } from 'react-html5-camera-photo';
-import 'react-html5-camera-photo/build/css/index.css';
 
 class Account extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            debug: null,
-        }
-    }
-
-    onTakePhoto = (dataUri) => {
-        // Do stuff with the dataUri photo...
-        // console.log('takePhoto', dataUri);
-        this.setState({
-            debug: dataUri
-        })
     }
 
     render() {
         return (
             <div>
-                {
-                    this.state.debug 
-                    ?
-                    this.state.debug
-                    :
-                    <Camera
-                        isImageMirror={false}
-                        imageCompression = {0.70}
-                        onTakePhoto={(dataUri) => this.onTakePhoto(dataUri)}
-                        idealFacingMode={FACING_MODES.ENVIRONMENT}
-                    />
-                }
             </div>
         )
     }
