0edb5ad8b843dfa989089a41f6ca7dafdf0828aa
new5558 <new17353@gmail.com>
2019-04-21 04:49:09 +0700

    attempt to patch ios 12.2 open maps bug

--- pages/index.js
+++ pages/index.js
@@ -272,7 +272,8 @@ class App extends Component {
       ((navigator.platform.indexOf("iPhone") != -1) ||
       (navigator.platform.indexOf("iPad") != -1) ||
       (navigator.platform.indexOf("iPod") != -1))
-      window.open("maps://maps.google.com/maps?daddr=" + latitude + "," + longitude + "&amp;ll=");
+      // window.open("maps://maps.google.com/maps?daddr=" + latitude + "," + longitude + "&amp;ll=");
+      location.hash = "maps://maps.google.com/maps?daddr=" + latitude + "," + longitude + "&amp;ll=";
     else /* else use Google */
       window.open("https://maps.google.com/maps?daddr=" + latitude + "," + longitude + "&amp;ll=");
   }
