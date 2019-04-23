2ecc58087af8bac24ef73fa212ec6a524db2d943
new <new17353@gmail.com>
2019-04-23 15:50:06 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -227,7 +227,7 @@ class App extends Component {
       longitude = target.dataset.longitude;
       const { currentLatitude, currentLongitude, currentAccuracy } = this.state.currentLocation
       const distance = this.distance(latitude, longitude, currentLatitude, currentLongitude);
-      isInRange = distance < (0.5 + currentAccuracy / 1000);
+      isInRange = true;
       // isInRange = true;
     }
     if (name === "Success") {
